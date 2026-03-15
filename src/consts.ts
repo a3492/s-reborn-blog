1b421a5a569e5d20a43279e16f9310bb47be0e51
a3492 <a01034920591@gmail.com>
2026-03-15 15:30:33 +0900

    커스터마이징: 사이트 제목, 설명, 푸터 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'S-Reborn World';
+export const SITE_DESCRIPTION = 'AI of the people, by the people, for the people';
